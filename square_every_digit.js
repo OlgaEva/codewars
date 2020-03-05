de18e68760d6a8b9e3264e0a72618fc7dfdaf7d6
OlgaEva <olga.rosas@gmail.com>
2020-03-05 07:43:09 -0400

    sq_every_digit solution

--- square_every_digit.js
+++ square_every_digit.js
@@ -6,3 +6,15 @@ For example, if we run 9119 through the function, 811181 will come out, because 
 Note: The function accepts an integer and returns an integer
 
 FUNDAMENTALS    MATHEMATICS ALGORITHMS  LOGIC   NUMBERS */
+
+function squareEveryDigit(num) {
+    let stringed = JSON.stringify(num)
+    let into_arr = stringed.split('')
+    let new_arr = []
+        for (let chars of into_arr){
+            new_arr.push(parseInt(chars) * parseInt(chars))
+        }; 
+    return new_arr.join('')
+};
+
+squareEveryDigit(234);
