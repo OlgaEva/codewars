326ede04f18d807e65ab73316f66b6407d4cdd23
OlgaEva <olga.rosas@gmail.com>
2020-04-01 08:41:17 -0400

    add non repeating solution

---
+++ non_repeating.js
@@ -0,0 +1,30 @@
+// DIRECTIONS:
+
+// Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
+
+// For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+
+// As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+
+// If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
+
+// ALGORITHMS STRINGS SEARCH
+
+function nonRepeat(string){
+    let charMap = {}
+  
+    for(let char of string) {
+      if(!charMap[char]){
+        charMap[char] = 1;
+      }else{
+        charMap[char] +=1;
+      }
+    }
+    for(let elem in charMap){
+      if(charMap[elem] == 1) {
+        return elem; 
+      }
+    }
+  }
+  
+  
