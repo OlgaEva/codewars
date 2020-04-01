f61798db5c9555aba5f27bc2ce454aa6aad301b4
OlgaEva <olga.rosas@gmail.com>
2020-04-01 10:58:45 -0400

    add solution for bonus tests

--- non_repeating.js
+++ non_repeating.js
@@ -1,10 +1,14 @@
 // DIRECTIONS:
 
-// Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
+// Write a function named first_non_repeating_letter that takes a string input, and returns 
+//the first character that is not repeated anywhere in the string.
 
-// For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+// For example, if given the input 'stress', the function should return 't', since the 
+//letter t only occurs once in the string, and occurs first in the string.
 
-// As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+// As an added challenge, upper- and lowercase letters are considered the same character, 
+//but the function should return the correct case for the initial letter. For example, 
+//the input 'sTreSS' should return 'T'.
 
 // If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
 
@@ -12,19 +16,26 @@
 
 function nonRepeat(string){
     let charMap = {}
+    let arr = string.split('')
+
+    for(let char of string.toLowerCase()) {
+        if(!charMap[char]){
+          charMap[char] = 1;
+        }else{
+          charMap[char] +=1;
+       }
+     }
   
-    for(let char of string) {
-      if(!charMap[char]){
-        charMap[char] = 1;
-      }else{
-        charMap[char] +=1;
-      }
-    }
     for(let elem in charMap){
-      if(charMap[elem] == 1) {
-        return elem; 
-      }
-    }
+        if(charMap[elem] == 1) {
+           if(arr.includes(elem)){
+              return elem; 
+           } else {
+              return elem.toUpperCase();
+        }
+     }
   }
+    return '';
+}
   
   
