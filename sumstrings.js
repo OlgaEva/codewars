619376afb3442f9d9a3610d1077090e268ee39b5
OlgaEva <olga.rosas@gmail.com>
2020-04-23 15:54:10 -0400

    add alternate solution to sum Strings as Numbers

--- sumstrings.js
+++ sumstrings.js
@@ -35,11 +35,43 @@ function sumStrings(a, b){
     return summedUp.toString();
   }
   
-  // sumStrings("", "9")
-  
-  // sumStrings('712569312664357328695151392', '8100824045303269669937')
-  
-  //  Expected: '712577413488402631964821329', instead got: '7.125774134884027e+26'
-//   sumStrings('50095301248058391139327916261', '81055900096023504197206408605') 
-  
-  // - Expected: '131151201344081895336534324866', instead got: '1.3115120134408189e+29'
+
+
+  /*
+  function add(str1, str2) {
+
+    let sum = "";  // our result will be stored in a string.
+
+    // we'll need these in the program many times.
+    let str1Length = str1.length;
+    let str2Length = str2.length;
+
+    // if s2 is longer than s1, swap them.
+    if(str2Length > str1Length ){
+        let temp = str2;
+        str2 = str1;
+        str1 = temp;
+    }
+
+    let carry = 0;  // number that is carried to next decimal place, initially zero.
+    let a;
+    let b;
+    let temp;
+    let digitSum;
+    for (let i = 0; i < str1.length; i++) {
+        a = parseInt(str1.charAt(str1.length - 1 - i));      // get ith digit of str1 from right, we store it in a
+        b = parseInt(str2.charAt(str2.length - 1 - i));      // get ith digit of str2 from right, we store it in b
+        b = (b) ? b : 0;                                    // make sure b is a number, (this is useful in case, str2 is shorter than str1
+        temp = (carry + a + b).toString();                  // add a and b along with carry, store it in a temp string.
+        digitSum = temp.charAt(temp.length - 1);            //
+        carry = parseInt(temp.substr(0, temp.length - 1));  // split the string into carry and digitSum ( least significant digit of abSum.
+        carry = (carry) ? carry : 0;                        // if carry is not number, make it zero.
+
+        sum = (i === str1.length - 1) ? temp + sum : digitSum + sum;  // append digitSum to 'sum'. If we reach leftmost digit, append abSum which includes carry too.
+
+    }
+
+    return sum;     // return sum
+
+}
+*/
