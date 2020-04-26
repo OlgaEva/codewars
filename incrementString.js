1264a013b845a7f0270fb28c20cd8baa2488ae03
OlgaEva <olga.rosas@gmail.com>
2020-04-26 06:05:37 -0400

    add increment String solution

--- incrementString.js
+++ incrementString.js
@@ -13,5 +13,50 @@ foo099 -> foo100. <-----------
 
 Attention: If the number has leading zeros the amount of digits should be considered.
 
-FUNDAMENTALSREGULAR EXPRESSIONSDECLARATIVE PROGRAMMINGADVANCED LANGUAGE FEATURESSTRINGSPARSINGALGORITHMS
+FUNDAMENTALS REGULAR EXPRESSIONS DECLARATIVE PROGRAMMING ADVANCED LANGUAGE FEATURES STRINGS PARSING ALGORITHMS
 */
+
+
+function incrementString(str){
+    //create useful variables; empty string to store the incremented one, newArr to hold array-version of input, 
+    //a stringified-number arr and empty arr
+    let newStr = '';
+    let newArr = str.split('');
+    let numArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    let empty = [];
+  
+    //check if last char is number, if so, go through the array from last to first putting nums in their own arr
+    if(numArr.includes(newArr[newArr.length-1])){
+      for(let i = newArr.length - 1; i >= 0; i--){
+        if(numArr.includes(newArr[i])){
+          empty.push(newArr[i]);
+        }
+      }
+      let size = empty.length;
+      let newEnd = parseInt(empty.reverse().join('')) + 1;
+      let count = newEnd.toString().length;
+      let old = (newArr.slice(0, newArr.length - size));
+
+      //deal with leading zeros
+        if(size === count){    
+          newStr = old.join('').concat(newEnd)
+        }else{
+          let amtZeros = size - count;
+          let temp = [];
+  
+          while(amtZeros > 0) {
+            temp.push('0');
+            amtZeros --;
+          }
+          numEnd = temp.concat(newEnd)
+  
+          newStr = old.concat(numEnd).join('')
+        }
+  
+        // if last char is letter, add a '1' and be done
+    }else{
+        newArr.push('1')
+        newStr = newArr.join('')
+      }
+      return newStr;
+    }
