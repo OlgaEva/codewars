24d6f469be7d71394cf957285787c5f96f28313a
OlgaEva <olga.rosas@gmail.com>
2020-06-17 15:53:55 -0400

    add next_bigger.js

---
+++ next_bigger.js
@@ -0,0 +1,25 @@
+/* Directions
+Next bigger number with the same digits
+208233794% of 2,3295,024 of 13,924GiacomoSorbi
+JavaScript
+Description:
+Create a function that takes a positive integer and returns the next bigger number that can be formed by rearranging its digits. For example:
+12 ==> 21
+513 ==> 531
+2017 ==> 2071
+nextBigger(num: 12)   // returns 21
+nextBigger(num: 513)  // returns 531
+nextBigger(num: 2017) // returns 2071
+If the digits can't be rearranged to form a bigger number, return -1 (or nil in Swift):
+9 ==> -1
+111 ==> -1
+531 ==> -1
+nextBigger(num: 9)   // returns nil
+nextBigger(num: 111) // returns nil
+nextBigger(num: 531) // returns nil
+REFACTORING NUMBERS STRINGS INTEGERS
+*/
+
+function nextBigger(n){
+    
+}
